2f5739808f7fc184c35dda9158d90018ddc98456
glafira <justfirka@gmail.com>
2024-10-13 15:43:35 +0300

    Добавлены функции для постраения маршрута до аудитории в корпусе на Большой Печерской

--- вшэська.py
+++ вшэська.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+import random
 bot = telebot.TeleBot('8179560224:AAF6aFKzEp6zJN1s31whhtHB-ABgKDzzv_E')
 
 @bot.message_handler(commands=['start'])
@@ -21,6 +22,9 @@ def building(message):
     if message.text == 'на Львовской':
         bot.send_message(message.chat.id, 'Напишите номер аудитории.')
         bot.register_next_step_handler(message, livovka_handler)
+    if message.text == 'на Большой Печерской':
+        bot.send_message(message.chat.id, 'Напишите номер аудитории.')
+        bot.register_next_step_handler(message, pecherskaya_handler)
 
 def livovka_handler(message):
     audience_of_livovka = {}
@@ -31,5 +35,16 @@ def livovka_handler(message):
     user_input = message.text
     response = audience_of_livovka.get(str(user_input), "Аудитория не найдена.")
     bot.send_message(message.chat.id, response)
-   
+
+
+def pecherskaya_handler(message):
+    audience_of_pecherskaya = {}
+    with open('audience_of_pecherskaya.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            key, value = line.strip().split(': ')
+            audience_of_pecherskaya[key] = value
+    user_input = message.text
+    response = audience_of_pecherskaya.get(str(user_input), "Аудитория не найдена.")
+    bot.send_message(message.chat.id, response)
+
 bot.infinity_polling()
