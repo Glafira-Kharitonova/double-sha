295c39f2b1eb282dbe8b91c7596c677c38978be0
User <marinavoodrmova@gmail.com>
2024-10-20 03:24:18 +0500

    Изменено reply to на send messege

--- вшэська.py
+++ вшэська.py
@@ -3,13 +3,6 @@ import random
 
 bot = telebot.TeleBot('8179560224:AAF6aFKzEp6zJN1s31whhtHB-ABgKDzzv_E')
 
-@bot.message_handler(content_types=['text'])
-def start_message(message):
-    with open('start_message.txt', 'r', encoding='utf-8') as file:
-        answer = file.read()
-    if message.text == "/start":
-        bot.send_message(message.chat.id, answer)
-
 # Функция для загрузки сообщений для i am sad из файла
 def load_support_messages(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -22,9 +15,15 @@ support_messages = load_support_messages('support_messages.txt')
 @bot.message_handler(commands=['iamsad'])
 def send_support_message(message):
     support_message = random.choice(support_messages)
-    bot.reply_to(message, support_message)
+    bot.send_message(message.chat.id, support_message)
 
 
+@bot.message_handler(content_types=['text'])
+def start_message(message):
+    with open('start_message.txt', 'r', encoding='utf-8') as file:
+        answer = file.read()
+    if message.text == "/start":
+        bot.send_message(message.chat.id, answer)
 
 
 bot.infinity_polling()
