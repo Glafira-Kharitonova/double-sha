02d47bcc83c15dee637c68fd203c0c562db0f2e6
User <marinavoodrmova@gmail.com>
2024-10-20 02:37:33 +0500

    Команда iamsad

--- вшэська.py
+++ вшэська.py
@@ -3,9 +3,6 @@ import random
 
 bot = telebot.TeleBot('8179560224:AAF6aFKzEp6zJN1s31whhtHB-ABgKDzzv_E')
 
-#ФАЙЛЫ
-SUPPORT_MESSAGES = 'support_messages.txt'
-
 @bot.message_handler(content_types=['text'])
 def start_message(message):
     with open('start_message.txt', 'r', encoding='utf-8') as file:
@@ -18,6 +15,14 @@ def load_support_messages(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         messages = file.readlines()
     return [message.strip() for message in messages]
+
+support_messages = load_support_messages('support_messages.txt')
+
+# Обработчик команды /iamsad
+@bot.message_handler(commands=['iamsad'])
+def send_support_message(message):
+    support_message = random.choice(support_messages)
+    bot.reply_to(message, support_message)
 
 
 
