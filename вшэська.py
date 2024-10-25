5098976e9b76d6e16af1de3a43492a19815c6cf6
glafira <justfirka@gmail.com>
2024-10-25 15:47:57 +0300

    оптимизированна функция iamsad

--- вшэська.py
+++ вшэська.py
@@ -1,20 +1,14 @@
 import telebot
+from telebot import types
 import random
 bot = telebot.TeleBot('8179560224:AAF6aFKzEp6zJN1s31whhtHB-ABgKDzzv_E')
-
-# Функция для загрузки сообщений для i am sad из файла
-def load_support_messages(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        messages = file.readlines()
-    return [message.strip() for message in messages]
-
-support_messages = load_support_messages('support_messages.txt')
 
 # Обработчик команды /iamsad
 @bot.message_handler(commands=['iamsad'])
 def send_support_message(message):
-    support_message = random.choice(support_messages)
-    bot.send_message(message.chat.id, support_message)
+    with open('support_messages.txt', 'r', encoding='utf-8') as file:
+        support_message = [line for line in file.readlines()]
+    bot.send_message(message.chat.id, random.choice(support_messages))
 
 
 @bot.message_handler(content_types=['text'])
